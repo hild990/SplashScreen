0597fa41b00e5b49eb33ab1dfb42a74dd0e68479
logonrm <logonrm@fiap.com.br>
2017-02-10 09:34:10 -0200

    Cria8;72R~ao do slashscreen

---
+++ app/src/main/java/br/com/hild990/splashscreen/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package br.com.hild990.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
